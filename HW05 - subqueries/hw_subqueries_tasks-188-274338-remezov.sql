f3cfc40be0eedeb9c2a5fb6f8045f05519d3fe15
rsnlearn <rsn.learn@gmail.com>
2023-02-27 23:22:13 +0300

    add HW05 - subqueries and HW06 - window functions

---
+++ HW05 - subqueries/hw_subqueries_tasks-188-274338-remezov.sql
@@ -0,0 +1,322 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "03 - Подзапросы, CTE, временные таблицы".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- Для всех заданий, где возможно, сделайте два варианта запросов:
+--  1) через вложенный запрос
+--  2) через WITH (для производных таблиц)
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Выберите сотрудников (Application.People), которые являются продажниками (IsSalesPerson), 
+и не сделали ни одной продажи 04 июля 2015 года. 
+Вывести ИД сотрудника и его полное имя. 
+Продажи смотреть в таблице Sales.Invoices.
+*/
+
+--TODO: напишите здесь свое решение
+--  1) через вложенный запрос
+SELECT
+	p.PersonID
+	,p.FullName
+FROM
+	[Application].People p
+WHERE
+	p.IsSalesperson = 1
+	AND (SELECT COUNT(*) FROM Sales.Invoices WHERE InvoiceDate = '20150704' AND SalespersonPersonID = p.PersonID) = 0
+ORDER BY FullName
+
+--  2) через WITH (для производных таблиц)
+;WITH
+	SalesPersons AS (
+		SELECT 
+			PersonID
+			,FullName
+		FROM
+			[Application].People
+		WHERE
+			IsSalesperson = 1)
+	,DaySales AS (
+		SELECT
+			COUNT(*) AS InvoicesCount
+			,SalespersonPersonID
+		FROM Sales.Invoices
+		WHERE InvoiceDate = '20150704'
+		GROUP BY (SalespersonPersonID)
+		HAVING COUNT(*)  > 0)
+SELECT
+	PersonID
+	,FullName
+FROM
+	SalesPersons
+LEFT JOIN
+	DaySales
+	ON DaySales.SalespersonPersonID = SalesPersons.PersonID
+WHERE
+	DaySales.InvoicesCount IS NULL
+ORDER BY SalesPersons.FullName
+
+/*
+2. Выберите товары с минимальной ценой (подзапросом). Сделайте два варианта подзапроса. 
+Вывести: ИД товара, наименование товара, цена.
+*/
+
+--TODO: напишите здесь свое решение
+--  1) через вложенный запрос
+--  вложенный запрос 1
+SELECT
+	StockItemID
+	,StockItemName
+	,UnitPrice
+FROM
+	Warehouse.StockItems
+WHERE
+	UnitPrice = (SELECT MIN(UnitPrice) FROM Warehouse.StockItems)
+
+--  вложенный запрос 2
+SELECT
+	StockItemID
+	,StockItemName
+	,UnitPrice
+FROM (
+	SELECT
+		StockItemID
+		,StockItemName
+		,UnitPrice
+		,RANK() OVER (ORDER BY UnitPrice) AS UnitPriceRank
+	FROM
+		Warehouse.StockItems) siupr
+WHERE
+	UnitPriceRank = 1
+
+--  2) через WITH (для производных таблиц)
+;WITH
+	siupr AS (
+		SELECT
+			StockItemID
+			,StockItemName
+			,UnitPrice
+			,RANK() OVER (ORDER BY UnitPrice) AS UnitPriceRank
+		FROM
+			Warehouse.StockItems
+		)
+SELECT
+	StockItemID
+	,StockItemName
+	,UnitPrice
+FROM siupr
+WHERE
+	UnitPriceRank = 1
+
+/*
+3. Выберите информацию по клиентам, которые перевели компании пять максимальных платежей 
+из Sales.CustomerTransactions. 
+Представьте несколько способов (в том числе с CTE). 
+*/
+
+--TODO: напишите здесь свое решение
+-- через оконные функции
+SELECT DISTINCT
+	CustomerID
+	,CustomerName
+FROM
+	(SELECT
+		ct.CustomerID
+		,c.CustomerName
+		,ROW_NUMBER() OVER (ORDER BY ct.TransactionAmount DESC) AS TransactionRowRank
+	FROM 
+		Sales.CustomerTransactions ct
+	INNER JOIN
+		Sales.Customers c
+		ON c.CustomerID = ct.CustomerID) ctr
+WHERE 
+	TransactionRowRank <= 5
+
+--  1) через вложенный запрос
+SELECT DISTINCT
+	t5tc.CustomerID
+	,c.CustomerName
+FROM (
+	SELECT TOP 5
+		ct.CustomerID
+	FROM
+		Sales.CustomerTransactions ct
+	ORDER BY
+		ct.TransactionAmount DESC) t5tc
+INNER JOIN
+	Sales.Customers c
+	ON c.CustomerID = t5tc.CustomerID
+	
+--  2) через WITH (для производных таблиц)
+;WITH
+	t5tc AS(
+		SELECT TOP 5
+		ct.CustomerID
+	FROM
+		Sales.CustomerTransactions ct
+	ORDER BY
+		ct.TransactionAmount DESC)
+SELECT DISTINCT
+	t5tc.CustomerID
+	,c.CustomerName
+FROM
+	t5tc
+LEFT JOIN
+	Sales.Customers c
+	ON c.CustomerID = t5tc.CustomerID
+
+/*
+4. Выберите города (ид и название), в которые были доставлены товары, 
+входящие в тройку самых дорогих товаров, а также имя сотрудника, 
+который осуществлял упаковку заказов (PackedByPersonID).
+*/
+
+--TODO: напишите здесь свое решение
+--  1) через вложенный запрос
+SELECT DISTINCT
+	cu.DeliveryCityID
+	,ci.CityName AS DeliveryCityName
+	,p.FullName AS PackedByPersonName
+FROM
+	Sales.Invoices i
+INNER JOIN
+	Sales.InvoiceLines il
+	ON il.InvoiceID = i.InvoiceID
+INNER JOIN
+	(SELECT TOP 3
+		StockItemID
+	FROM
+		Warehouse.StockItems
+	ORDER BY
+		UnitPrice DESC) t3si
+	ON t3si.StockItemID = il.StockItemID
+INNER JOIN
+	Sales.Customers cu
+	ON cu.CustomerID = i.CustomerID
+INNER JOIN
+	[Application].Cities ci
+	ON ci.CityID = cu.DeliveryCityID
+INNER JOIN
+	[Application].People p
+	ON p.PersonID = i.PackedByPersonID
+
+--  2) через WITH (для производных таблиц)
+;WITH
+	t3si AS(
+		SELECT TOP 3
+		StockItemID
+	FROM
+		Warehouse.StockItems
+	ORDER BY
+		UnitPrice DESC)
+SELECT DISTINCT
+	cu.DeliveryCityID
+	,ci.CityName AS DeliveryCityName
+	,p.FullName AS PackedByPersonName
+FROM
+	Sales.Invoices i
+INNER JOIN
+	Sales.InvoiceLines il
+	ON il.InvoiceID = i.InvoiceID
+INNER JOIN
+	t3si
+	ON t3si.StockItemID = il.StockItemID
+INNER JOIN
+	Sales.Customers cu
+	ON cu.CustomerID = i.CustomerID
+INNER JOIN
+	[Application].Cities ci
+	ON ci.CityID = cu.DeliveryCityID
+INNER JOIN
+	[Application].People p
+	ON p.PersonID = i.PackedByPersonID
+
+-- ---------------------------------------------------------------------------
+-- Опциональное задание
+-- ---------------------------------------------------------------------------
+-- Можно двигаться как в сторону улучшения читабельности запроса, 
+-- так и в сторону упрощения плана\ускорения. 
+-- Сравнить производительность запросов можно через SET STATISTICS IO, TIME ON. 
+-- Если знакомы с планами запросов, то используйте их (тогда к решению также приложите планы). 
+-- Напишите ваши рассуждения по поводу оптимизации. 
+
+-- 5. Объясните, что делает и оптимизируйте запрос
+
+SELECT 
+	Invoices.InvoiceID, 
+	Invoices.InvoiceDate,
+	(SELECT People.FullName
+		FROM Application.People
+		WHERE People.PersonID = Invoices.SalespersonPersonID
+	) AS SalesPersonName,
+	SalesTotals.TotalSumm AS TotalSummByInvoice, 
+	(SELECT SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice)
+		FROM Sales.OrderLines
+		WHERE OrderLines.OrderId = (SELECT Orders.OrderId 
+			FROM Sales.Orders
+			WHERE Orders.PickingCompletedWhen IS NOT NULL	
+				AND Orders.OrderId = Invoices.OrderId)	
+	) AS TotalSummForPickedItems
+FROM Sales.Invoices 
+	JOIN
+	(SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
+	FROM Sales.InvoiceLines
+	GROUP BY InvoiceId
+	HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
+		ON Invoices.InvoiceID = SalesTotals.InvoiceID
+ORDER BY TotalSumm DESC
+
+-- --
+
+--TODO: напишите здесь свое решение
+-- Запрос выводит список инвойсов с общей суммой > 27000
+-- Список содержит: Ид инвойса, Дату инвойса, Имя ответственного сотрудника, Сумму инвойса,  Сумму по полностью отгруженным заказам
+-- Список упорядочен по сумме инвойса по убыванию
+
+--Вариант на JOIN
+SELECT 
+	Invoices.InvoiceID, 
+	Invoices.InvoiceDate,
+	People.FullName AS SalesPersonName,
+	SalesTotals.TotalSumm AS TotalSummByInvoice, 
+	OrdersTotal.PickedSumm AS TotalSummForPickedItems
+FROM 
+	(SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
+	FROM Sales.InvoiceLines
+	GROUP BY InvoiceId
+	HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
+INNER JOIN
+	Sales.Invoices
+		ON Invoices.InvoiceID = SalesTotals.InvoiceID
+INNER JOIN
+	[Application].People
+		ON People.PersonID = Invoices.SalespersonPersonID
+LEFT JOIN
+	(SELECT OrderLines.OrderId, SUM(OrderLines.PickedQuantity * OrderLines.UnitPrice) AS PickedSumm
+	FROM Sales.OrderLines
+	INNER JOIN
+		Sales.Orders
+		ON OrderLines.OrderId = Orders.OrderId
+	WHERE Orders.PickingCompletedWhen IS NOT NULL
+	GROUP BY OrderLines.OrderId) OrdersTotal
+		ON OrdersTotal.OrderId = Invoices.OrderId
+ORDER BY TotalSummByInvoice DESC
+
+-- производительность не изменилась, основные затраты на чтение из таблицы Invoices (Table 'Invoices'. Scan count 3, logical reads 11994)
+-- количество чтений совпадает
---
+++ HW06 - window functions/homework_window_functions_remezov.sql
@@ -0,0 +1,254 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "06 - Оконные функции".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+/*
+1. Сделать расчет суммы продаж нарастающим итогом по месяцам с 2015 года 
+(в рамках одного месяца он будет одинаковый, нарастать будет в течение времени выборки).
+Выведите: id продажи, название клиента, дату продажи, сумму продажи, сумму нарастающим итогом
+
+Пример:
+-------------+----------------------------
+Дата продажи | Нарастающий итог по месяцу
+-------------+----------------------------
+ 2015-01-29  | 4801725.31
+ 2015-01-30	 | 4801725.31
+ 2015-01-31	 | 4801725.31
+ 2015-02-01	 | 9626342.98
+ 2015-02-02	 | 9626342.98
+ 2015-02-03	 | 9626342.98
+Продажи можно взять из таблицы Invoices.
+Нарастающий итог должен быть без оконной функции.
+*/
+
+--напишите здесь свое решение
+SELECT
+	i.InvoiceID
+	,c.CustomerName
+	,i.InvoiceDate
+	,il.Quantity * il.UnitPrice AS UnitSum
+	,(  SELECT
+			SUM(sil.Quantity * sil.UnitPrice)
+		FROM 
+			Sales.Invoices si
+		INNER JOIN
+			Sales.InvoiceLines sil
+			ON sil.InvoiceID = si.InvoiceID
+		WHERE
+			si.InvoiceDate BETWEEN N'20150101' AND EOMONTH(i.InvoiceDate)
+	) AS MonthSalesTotalCumulative
+FROM 
+	 Sales.Invoices i
+INNER JOIN
+	Sales.InvoiceLines il
+ON il.InvoiceID = i.InvoiceID
+INNER JOIN
+	Sales.Customers c
+ON c.CustomerID = i.CustomerID
+WHERE i.InvoiceDate >= N'20150101'
+ORDER BY i.InvoiceDate, InvoiceID, UnitSum DESC;
+
+/*
+2. Сделайте расчет суммы нарастающим итогом в предыдущем запросе с помощью оконной функции.
+   Сравните производительность запросов 1 и 2 с помощью set statistics time, io on
+*/
+
+--напишите здесь свое решение
+SELECT
+	i.InvoiceID
+	,c.CustomerName
+	,i.InvoiceDate
+	,il.Quantity * il.UnitPrice AS UnitSum
+	,SUM(il.Quantity * il.UnitPrice) OVER (ORDER BY YEAR(i.InvoiceDate), MONTH(i.InvoiceDate)) AS MonthSalesTotalCumulative
+FROM 
+	 Sales.Invoices i
+INNER JOIN
+	Sales.InvoiceLines il
+ON il.InvoiceID = i.InvoiceID
+INNER JOIN
+	Sales.Customers c
+ON c.CustomerID = i.CustomerID
+WHERE i.InvoiceDate >= N'20150101'
+ORDER BY i.InvoiceDate, InvoiceID, UnitSum DESC;
+
+/*
+3. Вывести список 2х самых популярных продуктов (по количеству проданных) 
+в каждом месяце за 2016 год (по 2 самых популярных продукта в каждом месяце).
+*/
+
+--напишите здесь свое решение
+SELECT
+	st.InvoiceMonth
+	,st.StockItemName
+	,st.SalesQuantityTotal
+	,st.SalesRank
+FROM (
+	SELECT
+		MONTH(i.InvoiceDate) AS InvoiceMonth
+		,si.StockItemName
+		,SUM(il.Quantity) AS SalesQuantityTotal
+		,ROW_NUMBER() OVER (PARTITION BY MONTH(i.InvoiceDate) ORDER BY SUM(il.Quantity) DESC, si.StockItemName) AS SalesRank
+	FROM 
+		 Sales.Invoices i
+	INNER JOIN
+		Sales.InvoiceLines il
+		ON il.InvoiceID = i.InvoiceID
+	INNER JOIN
+		Warehouse.StockItems si
+		ON si.StockItemID = il.StockItemID
+	WHERE
+		i.InvoiceDate BETWEEN '20160101' AND '20161231'
+	GROUP BY MONTH(i.InvoiceDate), si.StockItemName) st
+WHERE st.SalesRank <= 2
+ORDER BY st.InvoiceMonth, st.SalesRank
+
+/*
+4. Функции одним запросом
+Посчитайте по таблице товаров (в вывод также должен попасть ид товара, название, брэнд и цена):
+* пронумеруйте записи по названию товара, так чтобы при изменении буквы алфавита нумерация начиналась заново
+* посчитайте общее количество товаров и выведете полем в этом же запросе
+* посчитайте общее количество товаров в зависимости от первой буквы названия товара
+* отобразите следующий id товара исходя из того, что порядок отображения товаров по имени 
+* предыдущий ид товара с тем же порядком отображения (по имени)
+* названия товара 2 строки назад, в случае если предыдущей строки нет нужно вывести "No items"
+* сформируйте 30 групп товаров по полю вес товара на 1 шт
+
+Для этой задачи НЕ нужно писать аналог без аналитических функций.
+*/
+
+SELECT
+	StockItemID
+	,StockItemName
+	,Brand
+	,UnitPrice
+	,ROW_NUMBER() OVER(PARTITION BY UPPER(LEFT(StockItemName, 1)) ORDER BY StockItemName) AS RowNumByFirstChar
+	,COUNT(*) OVER() AS StockItemsCount
+	,COUNT(*) OVER(PARTITION BY UPPER(LEFT(StockItemName, 1)))  AS StockItemsCountByFirstChar
+	,LEAD(StockItemID) OVER(ORDER BY StockItemName) AS NextStockItemIdByName
+	,LAG(StockItemID) OVER(ORDER BY StockItemName) AS PrevStockItemIdByName
+	,LAG(StockItemName, 2, 'No items') OVER(ORDER BY StockItemName) AS PrevStockItemIdByName
+	,NTILE(30) OVER(ORDER BY TypicalWeightPerUnit) AS GroupByUnitWeight
+FROM
+	Warehouse.StockItems
+ORDER BY
+	StockItemName
+
+/*
+5. По каждому сотруднику выведите последнего клиента, которому сотрудник что-то продал.
+   В результатах должны быть ид и фамилия сотрудника, ид и название клиента, дата продажи, сумму сделки.
+*/
+
+SELECT 	 
+	SalespersonPersonID
+	,FullName
+	,CustomerName AS LastInvoiceCustomerName
+	,InvoiceDate AS LastInvoiceDate
+	,InvoiceSum AS LastInvoiceSum
+FROM
+	(SELECT
+		i.SalespersonPersonID
+		,p.FullName
+		,c.CustomerName
+		,i.InvoiceDate
+		,SUM(il.Quantity * il.UnitPrice) OVER (PARTITION BY i.InvoiceID) AS InvoiceSUM
+		,DENSE_RANK() OVER(PARTITION BY i.SalespersonPersonID ORDER BY i.InvoiceID DESC) AS SalePersonInvoiceRank
+		,ROW_NUMBER() OVER(PARTITION BY i.SalespersonPersonID, i.InvoiceID ORDER BY il.StockItemID) AS SalePersonInvoiceLineRank
+	FROM
+		Sales.Invoices i
+	INNER JOIN
+		Sales.InvoiceLines il
+		ON il.InvoiceID = i.InvoiceID
+	INNER JOIN
+		Sales.Customers c
+		ON c.CustomerID = i.CustomerID
+	INNER JOIN
+		Application.People p
+		ON p.PersonID = i.SalespersonPersonID) spil
+WHERE
+	spil.SalePersonInvoiceRank = 1
+	AND spil.SalePersonInvoiceLineRank = 1
+ORDER BY spil.FullName
+
+-- вариант без оконных функций
+SELECT
+	spli.SalespersonPersonID
+	,sp.FullName
+	,c.CustomerName AS LastInvoiceCustomerName
+	,i.InvoiceDate AS LastInvoiceDate
+	,ils.InvoiceSum AS LastInvoiceSum
+FROM
+	(SELECT SalespersonPersonID, MAX(InvoiceID) AS LastInvoiceID
+	FROM Sales.Invoices
+	GROUP BY SalespersonPersonID) spli
+INNER JOIN	
+	Sales.Invoices i
+	ON i.InvoiceID = spli.LastInvoiceID
+INNER JOIN
+	Sales.Customers c
+	ON c.CustomerID = i.CustomerID
+INNER JOIN
+	(SELECT PersonID, FullName FROM Application.People WHERE IsSalesperson = 1) sp
+	ON sp.PersonID = spli.SalespersonPersonID
+INNER JOIN
+	(SELECT InvoiceID, SUM(Quantity * UnitPrice) AS InvoiceSum FROM Sales.InvoiceLines GROUP BY InvoiceID) ils
+	ON ils.InvoiceID = spli.LastInvoiceID
+ORDER BY sp.FullName
+
+/*
+6. Выберите по каждому клиенту два самых дорогих товара, которые он покупал.
+В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки.
+*/
+
+SELECT 
+	CustomerID
+	,CustomerName
+	,StockItemID
+	,UnitPrice
+	,InvoiceDate
+FROM
+	(SELECT
+		CustomerID
+		,CustomerName
+		,StockItemID
+		,UnitPrice
+		,InvoiceDate
+		,SUM(CustomerItemRowRank) OVER(PARTITION BY cil.CustomerName ORDER BY UnitPrice DESC, InvoiceDate DESC) AS UqItemsCount
+	FROM (
+		SELECT
+			c.CustomerID
+			,c.CustomerName
+			,il.StockItemID
+			,il.UnitPrice
+			,i.InvoiceDate
+			,ROW_NUMBER() OVER(PARTITION BY c.CustomerName, il.StockItemID ORDER BY il.UnitPrice DESC, i.InvoiceDate DESC) AS CustomerItemRowRank
+		FROM
+			Sales.Invoices i
+		INNER JOIN
+			Sales.Customers c
+			ON c.CustomerID = i.CustomerID
+		INNER JOIN
+			Sales.InvoiceLines il
+			ON il.InvoiceID = i.InvoiceID
+		) cil
+	WHERE CustomerItemRowRank = 1) simp
+WHERE UqItemsCount <= 2
+ORDER BY CustomerName, UnitPrice DESC, StockItemID
+
+--Опционально можете для каждого запроса без оконных функций сделать вариант запросов с оконными функциями и сравнить их производительность. 
